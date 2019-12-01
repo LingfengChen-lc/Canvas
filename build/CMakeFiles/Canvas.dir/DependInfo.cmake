c1c8fe5e4dc8969aa7fadc2de47dc630b1b37e0e
Liam <lc3651@nyu.edu>
2019-12-01 12:24:09 -0500

    init commit

---
+++ build/CMakeFiles/Canvas.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/WangZheng/Desktop/Fall2019/CG/Canvas/src/helpers.cpp" "/Users/WangZheng/Desktop/Fall2019/CG/Canvas/build/CMakeFiles/Canvas.dir/src/helpers.cpp.o"
+  "/Users/WangZheng/Desktop/Fall2019/CG/Canvas/src/main.cpp" "/Users/WangZheng/Desktop/Fall2019/CG/Canvas/build/CMakeFiles/Canvas.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DATA_DIR=\"/Users/WangZheng/Desktop/Fall2019/CG/Canvas/data/\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext/glfw/include"
+  "../ext/glad/include"
+  "../ext/eigen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/WangZheng/Desktop/Fall2019/CG/Canvas/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/WangZheng/Desktop/Fall2019/CG/Canvas/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
